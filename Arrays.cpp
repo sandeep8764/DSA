622df6a71dbfb627d77c48e45202e45254cb7b74
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-06-29 22:50:03 +0530

    ARRAYS IN DSA

---
+++ Arrays.cpp
@@ -0,0 +1,31 @@
+// Arrays in DSA 
+
+#include <iostream>
+#include <stdio.h>
+using namespace std ;
+
+
+int main (){
+    int A[5];
+    int A1[5]={10,20,30,40,50};
+    int A2[5]={11,22,33};
+    int A3[5]={0};
+    int A4[]={1203,45,56,78,23};
+
+
+
+
+
+
+    for(int i=0;i<5;i++){
+        printf("%d\n",A1[i]);
+        printf("%u\n",&A1[i]);
+
+    }
+
+
+
+
+    return 0;
+     
+}
