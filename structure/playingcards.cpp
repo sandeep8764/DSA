71f8efe0a6c7e00b8f9e255d126ebdf455419652
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-07-01 12:42:55 +0530

    Playing cards In Structure

---
+++ structure/playingcards.cpp
@@ -0,0 +1,32 @@
+// Playing Cards Example
+#include <stdio.h> 
+#include <iostream> 
+ using namespace std ;
+  struct cards {
+     int face ;
+     int colour;
+     int shape ;
+      
+     
+  };
+   
+  int main (){
+     struct cards c;
+     c.face=1;
+     c.shape=0;
+     c.colour=0;
+
+      struct cards C={1,1,0};
+       
+
+
+
+     return 0;
+      
+
+
+
+  }
+
+
+
