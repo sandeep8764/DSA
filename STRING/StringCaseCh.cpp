b4efdeea546eb8e518f5264880a846837fb40898
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2025-07-03 00:30:58 +0530

    Upper Case To Lower case

---
+++ STRING/StringCaseCh.cpp
@@ -0,0 +1,21 @@
+// Change The case of Given String 
+
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+
+    char string []="WELCOME";
+    for(int i=0;string[i]!='\0';i++)
+    {
+         
+        printf("index:%d %c  \n",i,string[i]+32); // PRINT THE WELCOME OR STRING INDEX and also convert to loweer case 
+
+         
+         
+    }
+    return 0;
+     
+}
