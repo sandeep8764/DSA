408aaa9c1fc39362efe156b304b7b491bdfd7253
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2026-01-29 22:32:38 +0530

    Introduction to DLL

---
+++ Doubly_linked_list/intro.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    Node* prev;
+    int data;
+    Node* next;
+};
+
+Node* first = NULL;
+
+void create(int A[], int n) {
+    Node *t, *last;
+
+    first = new Node;
+    first->data = A[0];
+    first->prev = NULL;
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++) {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        t->prev = last;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(Node* p) {
+    while (p != NULL) {
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+int length(Node* p) {
+    int l = 0;
+    while (p != NULL) {
+        l++;
+        p = p->next;
+    }
+    return l;
+}
+
+int main() {
+    int A[] = {11, 22, 33, 44, 55};
+    create(A, 5);
+
+    cout << "Length of DLL: " << length(first) << endl;
+    Display(first);
+
+    return 0;
+}
