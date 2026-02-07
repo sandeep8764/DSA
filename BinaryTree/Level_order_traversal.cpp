cf88b1e66414322ae71b94d817587490eeff230e
Sandeep Yadav <sandeepyadav@Sandeeps-Air.mshome.net>
2026-02-07 15:20:28 +0530

    Level Order Traversal in tree

---
+++ BinaryTree/Level_order_traversal.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void levelOrderTraversal(Node* root) {
+    if (root == NULL) return;
+
+    queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        Node* curr = q.front();
+        q.pop();
+
+        cout << curr->data << " ";
+
+        if (curr->left != NULL)
+            q.push(curr->left);
+
+        if (curr->right != NULL)
+            q.push(curr->right);
+    }
+}
+
+int main() {
+    Node* root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    levelOrderTraversal(root);
+
+    return 0;
+}
