114b57196b39b3c6fdbe944217e065c407efdeef
Sandeep Yadav <sandeepyadav@Sandeeps-MacBook-Air.local>
2026-03-07 23:28:51 +0530

    MergeSort Tech

---
+++ Sorting_Technique/MergeSort.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for(int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for(int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+
+    while(i < n1 && j < n2) {
+        if(L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while(i < n1)
+        arr[k++] = L[i++];
+
+    while(j < n2)
+        arr[k++] = R[j++];
+}
+
+void iterativeMergeSort(int arr[], int n) {
+    for(int curr_size = 1; curr_size < n; curr_size *= 2) {
+        for(int left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
+            
+            int mid = min(left_start + curr_size - 1, n - 1);
+            int right_end = min(left_start + 2 * curr_size - 1, n - 1);
+
+            merge(arr, left_start, mid, right_end);
+        }
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    iterativeMergeSort(arr, n);
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
